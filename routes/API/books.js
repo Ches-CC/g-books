0c7dd4e62235ae74a65d7e01a897e3cbaeaa6710
Ches-CC <chesley.calloway@gmail.com>
2019-07-21 15:46:23 -0700

    CRUD operations backend setup

--- routes/API/books.js
+++ routes/API/books.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
+// ---===C R U D===---
+// (C)R(UD)
 router.route("/")
     .get(booksController.findAll);
+
+// C(RUD)
+router.route("/add")
+    .post(booksController.create);
+
+// (CR)U(D)
+router.route("/update")
+    .post(booksController.update);
+
+// (CRU)D
+router.route("/remove")
+    .get(booksController.remove);
 
 module.exports = router;
